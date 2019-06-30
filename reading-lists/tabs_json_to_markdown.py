0923ad811de3047ffdb60b30929345395f5f448f
Bradley Bossard <bradleybossard@gmail.com>
2019-06-30 12:42:08 -0700

    Add script for parsing tabs.json file

---
+++ reading-lists/tabs_json_to_markdown.py
@@ -0,0 +1,9 @@
+import json
+
+strings = []
+with open('tabs.json', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+    with open("links.md", "w") as text_file:
+        for p in data:
+            # print('title: ' + p['title'])
+            text_file.write("[{}]({})\n\n".format(p['title'], p['url']))
