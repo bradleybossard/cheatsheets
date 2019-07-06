70fa8aeca490f617ed19027b3b66d2eba0248c3a
Bradley Bossard <bradleybossard@gmail.com>
2019-07-06 13:03:03 -0700

    Adding TODOs for future work

--- reading-lists/move_links.sh
+++ reading-lists/move_links.sh
@@ -1,1 +1,6 @@
-cat links.md | grep -i $1 | awk -F: '{if(f!=$1)print ""; f=$1; print $0;}'
+# TODO: Write script show word counts from titles
+# TODO: Add space to each side of search, break out indiviual words
+# TODO: Make search take multiple terms
+
+cat links.md | grep -i " $1 " | awk -F: '{if(f!=$1)print ""; f=$1; print $0;}'
+#cat links.md | grep -i $1 | awk -F: '{if(f!=$1)print ""; f=$1; print $0;}'
