20b8a330de95f9263e7785f99efca347eae99cec
Bradley Bossard <bradleybossard@gmail.com>
2021-06-04 22:03:45 -0700

    update instructions for dumping chrome for android tabs

--- reading-lists/fetch_tabs_from_chrome_for_android.sh
+++ reading-lists/fetch_tabs_from_chrome_for_android.sh
@@ -1,2 +1,7 @@
+# make sure developer mode is enabled on phone
+# run the following to make sure phone is recognized
+# adb devices -l
+# chrome://inspect/#devices
+
 adb forward tcp:9222 localabstract:chrome_devtools_remote
 wget -O tabs.json http://0.0.0.0:9222/json/list
--- reading-lists/tabs_json_to_markdown.py
+++ reading-lists/tabs_json_to_markdown.py
@@ -1,9 +1,13 @@
 import json
+from datetime import date
 
-strings = []
+today = date.today()
+
+links_filename = 'links-' + str(today) + '.md'
+
 with open('tabs.json', encoding='utf-8') as json_file:
     data = json.load(json_file)
-    with open("links.md", "a") as text_file:
+    with open(links_filename, "a") as text_file:
         for p in data:
             # print('title: ' + p['title'])
             text_file.write("[{}]({})\n\n".format(p['title'], p['url']))
